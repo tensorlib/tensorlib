e4186141464f47a530d1bec3c4ed5555b33f25ef
Michael Eickenberg <michael.eickenberg@gmail.com>
2014-07-05 16:47:52 +0200

    typos

--- tensorlib/mathutils.py
+++ tensorlib/mathutils.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 def kr(B, C):
-    """Calculate the Khatari-Rao produc of 2D matrices."""
+    """Calculate the Khatri-Rao product of 2D matrices."""
     assert(len(B.shape) == len(C.shape) == 2)
     assert(B.shape[-1] == C.shape[-1])
     A = np.zeros((B.shape[0] * C.shape[0], B.shape[-1]))

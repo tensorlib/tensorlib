3e8991e87c908c8819b90f2c90cb833115f1c2de
Kyle Kastner <kastnerkyle@gmail.com>
2014-07-05 18:15:43 -0500

    Added correct test and moved np.kron form to _canonical_kr

--- tensorlib/mathutils.py
+++ tensorlib/mathutils.py
@@ -6,17 +6,25 @@ import numpy as np
 
 
 def kr(B, C):
-    """Calculate the Khatri-Rao product of 2D matrices. Assumes blocks to
-    be the columns of both matrices. 
-    See http://en.wikipedia.org/wiki/Kronecker_product#Khatri-Rao_product
+    """
+    Calculate the Khatri-Rao product of 2D matrices. Assumes blocks to
+    be the columns of both matrices.
+
+    See
+    http://gmao.gsfc.nasa.gov/events/adjoint_workshop-8/present/Friday/Dance.pdf
+    for more details.
 
     Parameters
-    ==========
+    ----------
 
-    B: ndarray, shape=(n, p)
-    C: ndarray, shape=(m, p)
-"""
+    B : ndarray, shape = [n, p]
+    C : ndarray, shape = [m, p]
 
+
+    Returns
+    -------
+    A : ndarray, shape = [m * n, p]
+    """
     if B.ndim != 2 or C.ndim != 2:
         raise ValueError("B and C must have 2 dimensions")
 
@@ -26,14 +34,16 @@ def kr(B, C):
     if p != pC:
         raise ValueError("B and C must have the same number of columns")
 
+    return np.einsum('ij, kj -> ikj', B, C).reshape(m * n, p)
+
+
+def _canonical_kr(B, C):
+    """
+    Internal implementation of vanilla kr product.
+    """
+    n, p = B.shape
+    m, pC = C.shape
     A = np.zeros((n * m, p))
     for k in range(B.shape[-1]):
         A[:, k] = np.kron(B[:, k], C[:, k])
     return A
-
-
-def _kr_einsum(B, C):
-
-    n, p = B.shape
-    m, pC = C.shape
-    return np.einsum('ij, kj -> ikj', B, C).reshape(m * n, p)
--- tensorlib/tests/test_mathutils.py
+++ tensorlib/tests/test_mathutils.py
@@ -1,33 +1,33 @@
 import numpy as np
 from numpy.testing import assert_array_almost_equal
-from tensorlib.mathutils import kr, _kr_einsum, _kr_npdot
+from tensorlib.mathutils import kr, _canonical_kr
+
+# http://gmao.gsfc.nasa.gov/events/adjoint_workshop-8/present/Friday/Dance.pdf
+B = np.arange(1, 5).reshape(2, 2)
+C = np.arange(5, 11).reshape(3, 2)
 
 
 def test_kr():
-    """Test correctness of Khatri-Rao product"""
-
-    B = np.eye(3)
-    C = np.arange(12).reshape(3, 4).T
-
+    """
+    Test correctness of Khatri-Rao product.
+    """
     A = kr(B, C)
 
-    expected_result = np.array([[0, 1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0],
-                                [0, 0, 0, 0, 4, 5, 6, 7, 0, 0, 0, 0],
-                                [0, 0, 0, 0, 0, 0, 0, 0, 8, 9, 10, 11]]).T
+    expected_result = np.array([[5, 12],
+                                [7, 16],
+                                [9, 20],
+                                [15, 24],
+                                [21, 32],
+                                [27, 40]])
 
     assert_array_almost_equal(A, expected_result)
 
 
-def test_kr_einsum_kr_equiv():
-    """Tests the equivalence of kr and _kr_einsum"""
-
-    rng = np.random.RandomState(42)
-    m, n, p = 4, 5, 6
-
-    B = rng.randn(n, p)
-    C = rng.randn(m, p)
-
+def test_canonical_kr():
+    """
+    Tests the equivalence of kr implementation and canonical np.kron form.
+    """
     A = kr(B, C)
-    A_einsum = _kr_einsum(B, C)
+    A_canon = _canonical_kr(B, C)
 
-    assert_array_almost_equal(A, A_einsum)
+    assert_array_almost_equal(A, A_canon)
